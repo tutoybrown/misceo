5e9c2c302cfb0e75e404b04d59b8adb411c97b20
tutoybrown <ekulmendoza@gmail.com>
2019-01-08 20:07:43 +0800

    Added Misceo First ver

---
+++ Misceo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 25.0f;
+    public float height = 10.0f;
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+    void FixedUpdate()
+    {
+        CharacterController playercontrol = GetComponent<CharacterController>();
+        //Height
+        playercontrol.height = height;
+        //Move Forward
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curspeed1 = speed * Input.GetAxis("Vertical");
+        playercontrol.SimpleMove(forward*curspeed1);
+        //Move Sideways
+        Vector3 side = transform.TransformDirection(Vector3.right);
+        float curspeed2 = speed * Input.GetAxis("Horizontal");
+        playercontrol.SimpleMove(side * curspeed2);
+
+    }
+}
+
