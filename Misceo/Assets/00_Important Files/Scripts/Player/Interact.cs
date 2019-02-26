a7a8077cf0d18680f36f2efbb42d0549d6ccd29c
EarlySummerRain <46488780+EarlySummerRain@users.noreply.github.com>
2019-02-26 18:13:00 +0800

    Update Interact.cs

--- Misceo/Assets/00_Important Files/Scripts/Player/Interact.cs
+++ Misceo/Assets/00_Important Files/Scripts/Player/Interact.cs
@@ -6,6 +6,9 @@ public class Interact
 {
     public void interactGlassware(GameObject carriedObject, GameObject interactedObject)
     {
+
+        GameObject object1;
+        GameObject object2;
         Debug.Log("You interacted with a Glassware");
         
     }
